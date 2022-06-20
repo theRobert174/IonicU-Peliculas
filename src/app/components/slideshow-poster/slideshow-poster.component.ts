5d35635351dc194c74161fe9cf9ca8f403b27ad3
Rodrigo Mtz <88547726+theRobert174@users.noreply.github.com>
2022-06-19 19:31:18 -0500

    Slides

---
+++ src/app/components/slideshow-poster/slideshow-poster.component.ts
@@ -0,0 +1,21 @@
+import { Component, Input, OnInit } from '@angular/core';
+import { Pelicula } from 'src/app/interfaces/interfaces';
+
+@Component({
+  selector: 'app-slideshow-poster',
+  templateUrl: './slideshow-poster.component.html',
+  styleUrls: ['./slideshow-poster.component.scss'],
+})
+export class SlideshowPosterComponent implements OnInit {
+
+  @Input() Peliculas : Pelicula[] = [];
+  slideOpts = {
+    slidesPerView: 3.3,
+    freeMode: true
+  }
+
+  constructor() { }
+
+  ngOnInit() {}
+
+}
